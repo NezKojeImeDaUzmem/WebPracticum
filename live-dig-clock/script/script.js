0ea3bddc4459e2159d8c8c9952dbb1bfae154e02
NezKojeImeDaUzmem <zvonimirsor@gmail.com>
2018-03-08 19:45:43 +0100

    did something

--- live-dig-clock/script/script.js
+++ live-dig-clock/script/script.js
@@ -24,16 +24,52 @@ $(document).ready(function(){
         min = parseFloat(min)
         sec = parseFloat(sec)
 
+
         var num
         if(h<12){
-            num = ((h*60 + min) / 2.58).floor()
+            num = ((h*60 + min) / 6.6)/100
         }else{
-            num = 255 - (((h-12)*60 + min) / 2.58)
+            num = (((h-12)*60 + min) / 6.6)/100
         }
-        num = parseInt(num)
-        var colors = 'rgb(' + num + ',' + num + ',' +num + ')'
-        $('main').css('backgroundColor', colors)
 
+        var white = 'rgba(0,0,0,' + num +')'
+        var blue = 'rgba(0,188,212,' + num +')'
+        var green = 'rgba(0,204,8,' + num +')'
+        var yellow = 'rgba(255,229,0,' + num +')'
+        var pink = 'rgba(233,30,99,' + num +')'
+
+        var currentColor = white
+
+        $('.backBrightness').css('backgroundColor', currentColor)
+
+        //color border between day and night
+        var darknessChange = 0.5
+
+        //info button
+        var info = $('.info')
+        if(num > darknessChange && info.hasClass('i-day')){
+            info.toggleClass('i-day').toggleClass('i-night')
+        }else if(num <= darknessChange && info.hasClass('i-night')){
+            info.toggleClass('i-day').toggleClass('i-night')
+        }
+
+
+        //info content
+        info.hover(function(){
+            info.find('.info-content').removeClass('none')
+        },function(){
+            info.find('.info-content').addClass('none')
+        })
+
+        //home button
+        var homeBtn = $('.linklist')
+        if(num > darknessChange && homeBtn.hasClass('linklist-day')){
+            console.log('night')
+            homeBtn.toggleClass('linklist-day').toggleClass('linklist-night')
+        }else if(num <= darknessChange && homeBtn.hasClass('linklist-night')){
+            console.log('day')
+            homeBtn.toggleClass('linklist-day').toggleClass('linklist-night')
+        }
     }
 
     nowTime()
